fe0a3b8c6f0c7bbe4563501a6124bfcec4ec98e4
Vadim Kosarev <mailkosarev@gmail.com>
2023-01-11 00:28:50 +0400

    faeture: added js logic; refactor: changed profile name and description overflow rules

---
+++ script/script.js
@@ -0,0 +1,40 @@
+let editProfileButton = document.querySelector('.profile__edit-button');
+let closeProfileButton = document.querySelector('.popup__close-button');
+let formElement = document.querySelector('.popup__form');
+
+initEventListeners();
+
+function initEventListeners() {
+  editProfileButton.addEventListener('click', openProfileEdit);
+  closeProfileButton.addEventListener('click', closeProfileEdit);
+  formElement.addEventListener('submit', updateProfileInfo);
+};
+
+function fillProfile() {
+  let name = document.querySelector('#name'),
+      description = document.querySelector('#description'),
+      profileName = document.querySelector('.profile__name').textContent,
+      profileDescription = document.querySelector('.profile__description').textContent;
+  name.value = profileName;
+  description.value = profileDescription;
+};
+
+function updateProfileInfo(evt) {
+  evt.preventDefault();
+  let name = document.querySelector('#name'),
+      description = document.querySelector('#description');
+  document.querySelector('.profile__name').textContent = name.value;
+  document.querySelector('.profile__description').textContent = description.value;
+  closeProfileEdit();
+};
+
+function openProfileEdit() {
+  let popupBlock = document.querySelector('.popup');
+  fillProfile();
+  popupBlock.classList.add('popup_opened');
+};
+
+function closeProfileEdit() {
+  let popupBlock = document.querySelector('.popup');
+  popupBlock.classList.remove('popup_opened');
+};
