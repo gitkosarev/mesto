2c2934f97976ae37e2081b3fe89b4e43b4ae91fc
Vadim Kosarev <mailkosarev@gmail.com>
2023-02-05 02:22:35 +0400

    feature: added card generator

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,55 @@
 const initialCards = [
   {
     name: 'Алтай',
-    link: './images/cards/altai.jpg'
+    link: './images/cards/altai.jpg',
+    alt: 'фото Алтай'
   },{
     name: 'Камчатка',
-    link: './images/cards/kamchatka.jpg'
+    link: './images/cards/kamchatka.jpg',
+    alt: 'фото Камчатка'
   },{
     name: 'Байкал',
-    link: './images/cards/baikal.jpg'
+    link: './images/cards/baikal.jpg',
+    alt: 'фото Байкал'
   },{
     name: 'Домбай',
-    link: './images/cards/dombai.png'
+    link: './images/cards/dombai.png',
+    alt: 'фото Домбай'
   },{
     name: 'Эльбрус',
-    link: './images/cards/elbrus.png'
+    link: './images/cards/elbrus.png',
+    alt: 'фото Эльбрус'
   },{
     name: 'Карачаевск',
-    link: './images/cards/karachaevsk.png'
+    link: './images/cards/karachaevsk.png',
+    alt: 'фото Карачаевск'
   }
 ];
 
 let editProfileButton = document.querySelector('.profile__edit-button'),
     closeProfileButton = document.querySelector('.popup__close-button'),
-    formElement = document.querySelector('.popup__form'),
-    heartElement = document.querySelector('.card__like-button');
+    formElement = document.querySelector('.popup__form');
 
 let nameInput = document.querySelector('.popup__input_value_name'),
     descriptionInput = document.querySelector('.popup__input_value_description'),
     profileName = document.querySelector('.profile__name'),
     profileDescription = document.querySelector('.profile__description'),
     popupBlock = document.querySelector('.popup');
+
+
+function initCards() {
+  if ('content' in document.createElement('template')) {
+    let cardsEl = document.querySelector('.cards');
+    let cardTemp = document.querySelector('#cardTemplate');
+    initialCards.forEach(function(item) {
+      let clone = cardTemp.content.cloneNode(true);
+      clone.querySelector('.card__image').src = item.link;
+      clone.querySelector('.card__image').alt = item.alt;
+      clone.querySelector('.card__title').textContent = item.name;
+      cardsEl.appendChild(clone);
+    });
+  }
+};
 
 function fillProfile() {
   nameInput.value = profileName.textContent;
@@ -59,4 +79,5 @@ function addSomeLove() {
 editProfileButton.addEventListener('click', openProfileEdit);
 closeProfileButton.addEventListener('click', closeProfileEdit);
 formElement.addEventListener('submit', updateProfileInfo);
-heartElement.addEventListener('click', addSomeLove);
+
+initCards();
